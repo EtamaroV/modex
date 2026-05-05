db87b4f71d0a0a04e2283dcc6c45893eca2fd4ae
Krittitee Ausavaditalert <TeamAusa@outlook.com>
2026-05-05 13:15:03 +0700

    2nd commit: fix: ProvinceQueue

---
+++ src/main/java/com/modex/modex/datastruct/ProvinceQueue.java
@@ -0,0 +1,44 @@
+package com.modex.modex.datastruct;
+
+public class ProvinceQueue{
+    public Province front;
+    public Province rear;
+
+    public ProvinceQueue(){
+        front = null;
+        rear = null;
+    }
+
+    public void enqueue(Province p){
+        if (this.front == null){
+            this.front = p;
+            this.rear = p;
+            return;
+        }
+
+        Province ptr = front;
+        while (ptr.QNext != null){
+            if (p.distanceFormSource < ptr.QNext.distanceFormSource){
+                p.QNext = ptr.QNext;
+                ptr.QNext = p;
+                return;
+            }
+        }
+        if (p == this.rear){
+            rear.QNext = p;
+            rear = p;
+            return;
+        }
+
+    }
+
+    public Province dequeue(){
+        Province p = front;
+        front = front.QNext;
+        return p;
+    }
+
+    public Province peek(){
+        return front;
+    }
+}
--- src/main/java/com/modex/modex/datastruct/Utility.java
+++ src/main/java/com/modex/modex/datastruct/Utility.java
@@ -9,51 +9,6 @@ import java.util.Collections;
 
 public class Utility {
 
-    public static class ProvinceQueue{
-        public Province front;
-        public Province rear;
-
-        public ProvinceQueue(){
-            front = null;
-            rear = null;
-        }
-
-        public void enqueue(Province p){
-            if (this.front == null){
-                this.front = p;
-                this.rear = p;
-                return;
-            }
-
-            Province ptr = front;
-            while (ptr.QNext != null){
-                if (p.distanceFormSource < ptr.QNext.distanceFormSource){
-                    p.QNext = ptr.QNext;
-                    ptr.QNext = p;
-                    return;
-                }
-            }
-            if (p == this.rear){
-                rear.QNext = p;
-                rear = p;
-                return;
-            }
-
-        }
-
-        public Province dequeue(){
-            Province p = front;
-            front = front.QNext;
-            return p;
-        }
-
-        public Province peek(){
-            return front;
-        }
-    }
-
-
-
     public static List<Edge> dijkstra(Province From, Province To){
         if (From == null || To == null) return null;
         if (From.edges == null) return null;
