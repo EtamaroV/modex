a9054997ce85c1d279d3409440bf903b0a12c1b8
Krittitee Ausavaditalert <TeamAusa@outlook.com>
2026-05-07 12:35:11 +0700

    new: Rider datastruct

--- src/main/java/com/modex/modex/datastruct/Rider.java
+++ src/main/java/com/modex/modex/datastruct/Rider.java
@@ -1,5 +1,42 @@
 package com.modex.modex.datastruct;
 
-public class Rider {
+import java.util.List;
 
+public class Rider {
+    public List<Parcel> parcelList;
+    public List<Edge> path;
+    public Province currentProvince;
+    public double latestTime;
+
+    public Rider(List<Parcel> p, Province province){
+        this.currentProvince = province;
+        this.parcelList = p;
+        this.path = null;
+    }
+
+    public void assignParcel(){
+        Province temp = parcelList.getFirst().getFrom();
+        for (Parcel p : parcelList){
+
+            if (p == parcelList.get(0)){
+                if (path.isEmpty()){
+                    path = Utility.dijkstra(p.getFrom(),p.getTo());
+                }
+                else{
+                    path.addAll(Utility.dijkstra(temp,p.getTo()));
+                }
+                temp = p.getTo();
+            }
+        }
+    }
+
+
+
+    public void removePackage(){
+        parcelList.removeFirst();
+    }
+
+    public void removeOldPath(){
+        path.removeFirst();
+    }
 }
